56b20646f9416a694871382e5df292042eb62633
KenanAlekberr <elekberovk442@gmail.com>
2025-11-19 00:25:12 +0400

    change prod yml and redis config and add actuator

--- src/main/java/com/example/usermanagement/config/redis/RedisConfigWithRedisson.java
+++ src/main/java/com/example/usermanagement/config/redis/RedisConfigWithRedisson.java
@@ -25,15 +25,28 @@ public class RedisConfigWithRedisson {
     @Value("${spring.data.redis.password}")
     String redisPassword;
 
+//    @Bean(destroyMethod = "shutdown")
+//    public RedissonClient redissonClient() {
+//        Config config = new Config();
+//        String redisUrl = "redis://" + redisHost + ":" + redisPort;
+//
+//        if (redisPassword != null && !redisPassword.isEmpty())
+//            config.useSingleServer().setAddress(redisUrl).setPassword(redisPassword);
+//        else
+//            config.useSingleServer().setAddress(redisUrl);
+//
+//        return Redisson.create(config);
+//    }
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
-        String redisUrl = "redis://" + redisHost + ":" + redisPort;
+        String url = "redis://" + redisHost + ":" + redisPort;
 
-        if (redisPassword != null && !redisPassword.isEmpty())
-            config.useSingleServer().setAddress(redisUrl).setPassword(redisPassword);
-        else
-            config.useSingleServer().setAddress(redisUrl);
+        var server = config.useSingleServer().setAddress(url);
+
+        if (!redisPassword.isBlank())
+            server.setPassword(redisPassword);
 
         return Redisson.create(config);
     }
