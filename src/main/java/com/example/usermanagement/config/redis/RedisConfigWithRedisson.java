5088d644e71a8e3007c2f8f21ebd2916c09b899a
KenanAlekberr <elekberovk442@gmail.com>
2025-11-18 16:03:13 +0400

    Update redis

--- src/main/java/com/example/usermanagement/config/redis/RedisConfigWithRedisson.java
+++ src/main/java/com/example/usermanagement/config/redis/RedisConfigWithRedisson.java
@@ -2,20 +2,38 @@ package com.example.usermanagement.config.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.experimental.FieldDefaults;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import static lombok.AccessLevel.PRIVATE;
 
 @Configuration
+@FieldDefaults(level = PRIVATE)
 public class RedisConfigWithRedisson {
-    @Bean
+    @Value("${spring.data.redis.host}")
+    String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    int redisPort;
+
+    @Value("${spring.data.redis.password:}")
+    String redisPassword;
+
+    @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        String redisUrl = "redis://" + redisHost + ":" + redisPort;
+
+        if (redisPassword != null && !redisPassword.isEmpty())
+            config.useSingleServer().setAddress(redisUrl).setPassword(redisPassword);
+        else
+            config.useSingleServer().setAddress(redisUrl);
 
         return Redisson.create(config);
     }
@@ -23,10 +41,27 @@ public class RedisConfigWithRedisson {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(WRITE_DATES_AS_TIMESTAMPS);
-
         return mapper;
     }
+
+
+//    @Bean
+//    public RedissonClient redissonClient() {
+//        Config config = new Config();
+//        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+//
+//        return Redisson.create(config);
+//    }
+//
+//    @Bean
+//    public ObjectMapper objectMapper() {
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        mapper.registerModule(new JavaTimeModule());
+//        mapper.disable(WRITE_DATES_AS_TIMESTAMPS);
+//
+//        return mapper;
+//    }
 }
